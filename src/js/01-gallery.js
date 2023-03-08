c528bd20ae093c75349de20fc165f2c7807bccb8
Lidiia Chyrnyshuk <l.mandych@gmail.com>
2023-03-08 14:09:29 +0200

    add task01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainerEl = document.querySelector('.gallery');
+const cardsMurkup = createImageCardsMurkup(galleryItems);
+// galleryContainerEl.insertAdjacentHTML('beforeend', cardsMurkup);
+galleryContainerEl.innerHTML = cardsMurkup;
+// створюю розмітку
+function createImageCardsMurkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li><a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a></li>`;
+    })
+    .join('');
+}
+console.log(createImageCardsMurkup(galleryItems));
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionPosition: 'bottom',
+  captionDelay: 250,
+  captionsData: 'alt',
+});
+
+lightbox.on('show.simplelightbox');
