6919797feaf866d71beb11fc12d24949cd5d3de5
Lidiia Chyrnyshuk <l.mandych@gmail.com>
2023-03-10 12:44:07 +0200

    add task_02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const localStorageKey = 'videoplayer-current-time';
+let currentTime = localStorage.getItem(localStorageKey);
+
+player.on('timeupdate', throttle(playback, 1000));
+
+function playback({ seconds }) {
+  localStorage.setItem(localStorageKey, seconds);
+}
+
+player.setCurrentTime(currentTime);
 
