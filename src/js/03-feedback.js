fe260511297c70ad4aeaaa7681e1f2d54501a0ed
Lidiia Chyrnyshuk <l.mandych@gmail.com>
2023-03-12 14:12:51 +0200

    add task_03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const inputEl = document.querySelector('input');
+const textareaEl = document.querySelector('textarea');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+const formData = {};
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+inputSavedData();
+
+function onFormInput(evt) {
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+   console.log(formData);
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  console.log(JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)));
+  evt.currentTarget.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+}
+
+function inputSavedData() {
+  const savedData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+
+  if (savedData) {
+    inputEl.value = savedData.email;
+    textareaEl.value = savedData.message;
+  }
+}
